71f78c129a53f83b7b0eaec54e9ba3962ffe6126
Jan lecoutere <jan.lecoutere@hotmail.be>
2018-12-14 21:11:20 +0100

    Background added
    Now you can see the background of the game

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
-from arcade import *
-
+import arcade
+import os
 #constants
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 1000
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+BACKGROUND = 0
 
-class Menu(arcade.Window):
-    SCREEN_HEIGHT
-    SCREEN_WIDTH
-    "test"
+class Bongogame(arcade.Window):
+    def __init__(self, width, height):
+        super().__init__(width, height, "Bongo Cat")
+        self.background = None
+    def setup(self):
+        self.background = arcade.load_texture("Assets/background0.png")
+    def on_draw(self):
+        arcade.start_render()
+        BACKGROUND = arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+    def update(self, delta_time):
+        pass
+    def on_key_press(self, key, key_modifiers):
+        pass
+    def on_key_release(self, key, key_modifiers):
+        pass
+    def on_mouse_motion(self, x,y,delta_x,delta_y):
+        pass
+    def on_mouse_press(self, x,y,delta_x,delta_y):
+        pass
+    def on_mouse_release(self, x,y,delta_x,delta_y):
+        pass
+def main():
+    game = Bongogame(SCREEN_WIDTH, SCREEN_HEIGHT)
+    game.setup()
+    arcade.run()        
 
-print("end")
+if __name__ == "__main__":
+    main()
